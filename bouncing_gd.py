43af2e5fb4f607238637a43c0753792aaebb792d
Omar <darkev777@gmail.com>
2024-12-10 23:16:39 +0400

    significant speed and performance optimization

--- bouncing_gd.py
+++ bouncing_gd.py
@@ -8,7 +8,7 @@ import numpy as np
 import torch.nn.functional as F
 from matplotlib import pyplot as plt
 
-plt.style.use('seaborn')
+# plt.style.use('seaborn')
 plt.rcParams['figure.autolayout'] = True
 
 norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -20,20 +20,20 @@ BS = 100
 LR = 10
 EPOCHS = 5
 
-# GD:
+# SGD:
 torch.manual_seed(0)
 np.random.seed(0)
 
 train_loader = DataLoader(train_data, batch_size=BS, shuffle=True, pin_memory=True)
-test_loader = DataLoader(test_data, batch_size=BS)
+test_loader = DataLoader(test_data, batch_size=BS, pin_memory=True)
 
 w = torch.randn(784, 100, requires_grad=True)
-b = torch.zeros(100, requires_grad=True)
+b = torch.randn(100, requires_grad=True)
 wh = torch.randn(100, 10, requires_grad=True)
-bh = torch.zeros(10, requires_grad=True)
+bh = torch.randn(10, requires_grad=True)
 
 
-losses_gd = []
+losses_sgd = []
 for _ in trange(EPOCHS):
     loss_batch = []
     for x, y in train_loader:
@@ -49,8 +49,8 @@ for _ in trange(EPOCHS):
             b -= grad_b * LR
             wh -= grad_wh * LR
             bh -= grad_bh * LR
-    losses_gd.append(np.mean(loss_batch))
-plt.semilogy(losses_gd, label='GD')
+    losses_sgd.append(np.mean(loss_batch))
+plt.semilogy(losses_sgd, label='SGD')
 
 acc = []
 with torch.no_grad():
@@ -67,12 +67,12 @@ torch.manual_seed(0)
 np.random.seed(0)
 
 train_loader = DataLoader(train_data, batch_size=BS, shuffle=True, pin_memory=True)
-test_loader = DataLoader(test_data, batch_size=BS)
+test_loader = DataLoader(test_data, batch_size=BS, pin_memory=True)
 
 w = torch.randn(784, 100, requires_grad=True)
-b = torch.zeros(100, requires_grad=True)
+b = torch.randn(100, requires_grad=True)
 wh = torch.randn(100, 10, requires_grad=True)
-bh = torch.zeros(10, requires_grad=True)
+bh = torch.randn(10, requires_grad=True)
 
 
 def dist(g1, g2):
@@ -87,9 +87,8 @@ def dist(g1, g2):
 TH = 0.9  # TH value is inversely proportional to "ini"
 shrink = 1.1
 LRw = torch.ones(100)*LR
-LRb = torch.ones(100)*LR
 LRwh = torch.ones(10)*LR
-LRbh = torch.ones(10)*LR
+
 losses_bgd = []
 for _ in trange(EPOCHS):
     loss_batch = []
@@ -104,16 +103,17 @@ for _ in trange(EPOCHS):
 
         # Find oracle weights:
         oracle_w = w - grad_w * LRw
-        oracle_b = b - grad_b * LRb
         oracle_wh = wh - grad_wh * LRwh
-        oracle_bh = bh - grad_bh * LRbh
+        
+        # Update bias vectors:
+        with torch.no_grad():
+            b -= grad_b * LR
+            bh -= grad_bh * LR
 
-        out = torch.softmax(torch.tanh(x @ oracle_w + oracle_b) @ oracle_wh + oracle_bh, dim=1)
+        out = torch.softmax(torch.tanh(x @ oracle_w + b) @ oracle_wh + bh, dim=1)
         loss = F.cross_entropy(out, y)
         grad_orc_w = torch.autograd.grad(loss, oracle_w, retain_graph=True)[0]  # (784, 100)
-        grad_orc_b = torch.autograd.grad(loss, oracle_b, retain_graph=True)[0]  # (100)
         grad_orc_wh = torch.autograd.grad(loss, oracle_wh, retain_graph=True)[0]  # (100, 10)
-        grad_orc_bh = torch.autograd.grad(loss, oracle_bh)[0]  # (10)
 
         with torch.no_grad():
             # Update w:
@@ -123,17 +123,9 @@ for _ in trange(EPOCHS):
                     d1, d2 = dist(g, g_orc)
                     if d1 > TH:
                         LRw[i] /= shrink
-                    w[:, i] = (w[:, i]*d1 + oracle_w[:, i]*d2)
+                    w[:, i] = w[:, i]*d1 + oracle_w[:, i]*d2
                 else:
                     w[:, i] = oracle_w[:, i] - g_orc * LRw[i]
-
-            # Update b:
-            for i, (g, g_orc) in enumerate(zip(grad_b, grad_orc_b)):  # (100)
-                if g * g_orc < -1:  # (1)
-                    b[i] = (b[i] + oracle_b[i]) / 2
-                    LRb[i] /= shrink
-                else:
-                    b[i] = oracle_b[i] - g_orc * LRb[i]
 
             # Update wh:
             for i, (g, g_orc) in enumerate(zip(grad_wh.T, grad_orc_wh.T)):  # (10, 100)
@@ -142,17 +134,9 @@ for _ in trange(EPOCHS):
                     d1, d2 = dist(g, g_orc)
                     if d1 > TH:
                         LRwh[i] /= shrink
-                    wh[:, i] = (wh[:, i]*d1 + oracle_wh[:, i]*d2)
+                    wh[:, i] = wh[:, i]*d1 + oracle_wh[:, i]*d2
                 else:
                     wh[:, i] = oracle_wh[:, i] - g_orc * LRwh[i]
-
-            # Update bh:
-            for i, (g, g_orc) in enumerate(zip(grad_bh, grad_orc_bh)):  # (100)
-                if g * g_orc < -1:  # (1)
-                    bh[i] = (bh[i] + oracle_bh[i]) / 2
-                    LRbh[i] /= shrink
-                else:
-                    bh[i] = oracle_bh[i] - g_orc * LRbh[i]
 
     losses_bgd.append(np.mean(loss_batch))
 plt.semilogy(losses_bgd, label='BGD')
